6d323b959f58c8cc4314ba005c26a9043108d225
Peter <113168584+prawstho@users.noreply.github.com>
2024-03-12 14:27:54 -0230

    Logins CRUD is done

--- routes/logins.js
+++ routes/logins.js
@@ -17,6 +17,32 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/:id', async (req, res) => {
+  // const aLogin = [
+  //    {id: 1, username: 'example', password: 'example'}
+  // ];
+  try {
+      let aLogin = await loginsDal.getLoginByLoginId(req.params.id); // from postgresql
+      if(DEBUG) console.table(aLogin);
+      if (aLogin.length === 0)
+          res.render('norecord')
+      else
+          res.render('login', {aLogin});
+  } catch {
+      res.render('503');
+  }
+});
+
+router.get('/:id/edit', async (req, res) => {
+  if(DEBUG) console.log('login.Edit : ' + req.params.id);
+  res.render('loginPatch.ejs', {username: req.query.username, theId: req.params.id});
+});
+
+router.get('/:id/delete', async (req, res) => {
+  if(DEBUG) console.log('login.Delete : ' + req.params.id);
+  res.render('loginDelete.ejs', {username: req.query.username, theId: req.params.id});
+});
+
 router.post('/', async (req, res) => {
   if(DEBUG) console.log("logins.POST");
   try {
@@ -26,6 +52,28 @@ router.post('/', async (req, res) => {
       // log this error to an error log file.
       res.render('503');
   } 
+});
+
+router.patch('/:id', async (req, res) => {
+  if(DEBUG) console.log('logins.PATCH: ' + req.params.id);
+  try {
+      await loginsDal.patchLogin(req.params.id, req.body.username, req.body.password);
+      res.redirect('/logins/');
+  } catch {
+      // log this error to an error log file.
+      res.render('503');
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  if(DEBUG) console.log('logins.DELETE: ' + req.params.id);
+  try {
+      await loginsDal.deleteLogin(req.params.id);
+      res.redirect('/logins/');
+  } catch {
+      // log this error to an error log file.
+      res.render('503');
+  }
 });
 
 module.exports = router
--- services/pg.logins.dal.js
+++ services/pg.logins.dal.js
@@ -18,6 +18,22 @@ var getLogins = function() {
   }); 
 };
 
+var getLoginByLoginId = function(id) {
+  if(DEBUG) console.log("logins.pg.dal.getLoginByLoginId()");
+  return new Promise(function(resolve, reject) {
+    const sql = `SELECT id, username, password FROM public."Logins" WHERE id = $1`;
+    dal.query(sql, [id], (err, result) => {
+      if (err) {
+        // logging should go here
+        if(DEBUG) console.log(err);
+        reject(err);
+      } else {
+        resolve(result.rows);
+      }
+    }); 
+  }); 
+};
+
 var addLogin = function(username, password) {
   if(DEBUG) console.log("logins.pg.dal.addLogin()");
   return new Promise(function(resolve, reject) {
@@ -34,7 +50,38 @@ var addLogin = function(username, password) {
   });
 };
 
+var patchLogin = function(id, username, password) {
+  if(DEBUG) console.log("logins.pg.dal.patchLogin()");
+  return new Promise(function(resolve, reject) {
+    const sql = `UPDATE public."Logins" SET username=$2, password=$3 WHERE id=$1;`;
+    dal.query(sql, [id, username, password], (err, result) => {
+      if (err) {
+          reject(err);
+        } else {
+          resolve(result.rows);
+        }
+    }); 
+  });
+};
+
+var deleteLogin = function(id) {
+  if(DEBUG) console.log("logins.pg.dal.deleteLogin()");
+  return new Promise(function(resolve, reject) {
+    const sql = `DELETE FROM public."Logins" WHERE id = $1;`;
+    dal.query(sql, [id], (err, result) => {
+      if (err) {
+          reject(err);
+        } else {
+          resolve(result.rows);
+        }
+    }); 
+  });
+};
+
 module.exports = {
   getLogins,
+  getLoginByLoginId,
   addLogin,
+  patchLogin,
+  deleteLogin,
 }
