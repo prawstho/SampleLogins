ed7271012835407bd5d84d083417ebb814a76852
Peter <113168584+prawstho@users.noreply.github.com>
2024-03-12 09:44:37 -0230

    fetch and add logins

---
+++ routes/logins.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const loginsDal = require('../services/pg.logins.dal')
+
+router.get('/', async (req, res) => {
+  // const theLogins = [
+  //     {id: 1, username: 'example', password: 'example'},
+  //     {id: 4, username: 'frodob', password: 'example'},
+  //     {id: 7, username: 'bilbob', password: 'example'}
+  // ];
+  try {
+      let theLogins = await loginsDal.getLogins(); 
+      if(DEBUG) console.table(theLogins);
+      res.render('logins', {theLogins});
+  } catch {
+      res.render('503');
+  }
+});
+
+router.post('/', async (req, res) => {
+  if(DEBUG) console.log("logins.POST");
+  try {
+      await loginsDal.addLogin(req.body.username, req.body.password);
+      res.redirect('/logins/');
+  } catch {
+      // log this error to an error log file.
+      res.render('503');
+  } 
+});
+
+module.exports = router
