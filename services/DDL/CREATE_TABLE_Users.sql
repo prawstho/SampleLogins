aca441c7dab00ecb02e720afe6e313802d617650
Peter <113168584+prawstho@users.noreply.github.com>
2024-03-20 09:29:58 -0230

    New user object with SQL for database tables / documents

---
+++ services/DDL/CREATE_TABLE_Users.sql
@@ -0,0 +1,11 @@
+create table public."users" (
+	_id INT,
+	first_name VARCHAR(24),
+	last_name VARCHAR(60),
+	username VARCHAR(24),
+	gender VARCHAR(12),
+	sms_phone VARCHAR(24),
+	profile_url VARCHAR(400),
+	join_date DATE,
+	birth_date DATE
+);
